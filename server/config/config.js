b29da69cdb686e7092c7b930e5ddaab6d874b03c
¨julian¨ <julian.fanjul.dev@gmail.com>
2021-01-03 15:31:00 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,9 +15,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://labase:o7qlPtSI2OxYTgxP@cluster0.ml1uu.mongodb.net/cafe';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 process.env.URLDB = urlDB;
