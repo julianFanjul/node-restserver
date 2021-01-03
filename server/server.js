c7e51bfb3e1fb9ecef56a89ecc7ee1431a114f92
¨julian¨ <julian.fanjul.dev@gmail.com>
2021-01-03 13:08:10 -0300

    restServer - Usuario

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 require('./config/config');
 
 const express = require('express');
+
+const mongoose = require('mongoose');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -12,37 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', (req, res) => {
-    res.json('get usuario')
-});
 
-app.post('/usuario', (req, res) => {
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
+mongoose.connect(process.env.URLDB,
+    { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+        if (err) throw err;
+        console.log('base de datos ONLINE');
     });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto:`, process.env.PORT);
